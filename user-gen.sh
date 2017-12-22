2b6a89e90e0dc9bce2d749d5423f61364017dce8
cy-ber <34772837+cy-ber@users.noreply.github.com>
2017-12-22 22:17:23 +0700

    Update user-gen.sh

--- user-gen.sh
+++ user-gen.sh
@@ -37,26 +37,23 @@ useradd -e `date -d "$masaaktif days" +"%Y-%m-%d"` -s /bin/false -M $uname
 exp="$(chage -l $uname | grep "Account expires" | awk -F": " '{print $2}')"
 echo -e "$pass\n$pass\n"|passwd $uname &> /dev/null
 echo -e ""| lolcat
-echo -e "|      Informasi Akun Baru SSH      |" | boxes -d dog | lolcat
-echo -e "===========-[[SERVER-PREMIUM]]-============" | lolcat
-echo -e "     Host: $MYIP" | lolcat
-echo -e "     Username: $uname" | lolcat
-echo -e "     Password: $pass                       " | lolcat
-echo -e "     Port default dropbear: 443,80         " | lolcat
-echo -e "     Port default openSSH : 22,143         " | lolcat
-echo -e "     Port default squid   : 8080,3128      " | lolcat
+echo -e "|       Informasi Akun Baru SSH      |" | boxes -d dog | lolcat
+echo -e "============[[-SERVER-PREMIUM-]]===========" | lolcat
+echo -e "     Host: $MYIP                           " | lolcat
+echo -e "     Username: $username                   " | lolcat
+echo -e "     Password: $password                   " | lolcat
+echo -e "     Port default dropbear: 777            " | lolcat
 echo -e "     Port default SSL/TLS : 443            " | lolcat
+echo -e "     Port default openSSH : 80             " | lolcat
+echo -e "     Port default squid   : 8080           " | lolcat
 echo -e "     Auto kill user maximal login 2        " | lolcat
 echo -e "-------------------------------------------" | lolcat
-echo -e "     Aktif Sampai: $exp                    " | lolcat
+echo -e "     Aktif Sampai: $(date -d "$AKTIF days" +"%d-%m-%Y")" | lolcat
 echo -e "===========================================" | lolcat
-echo -e "   DI LARANG:                              "| lolcat
-echo -e "   HACKING-DDOS-PHISING-SPAM-TORENT        " | lolcat
-echo -e "   CARDING-CRIMINAL CYBER.                 "| lolcat
+echo -e "     NO-CRIMINAL - CYBER,,,                " | lolcat
 echo -e "===========================================" | lolcat
-echo -e "   Script by YUSUF-ARDIANSYAH              "| lolcat
-echo -e "   Config OVPN:                            "| lolcat
-echo -e "   http://$MYIP:81/client.ovpn        " | lolcat
+echo -e "   http://$MYIP:81/client.ovpn             " | lolcat
+echo -e "   Script by YUSUF-ARDIANSYAH              " | lolcat
 echo -e "-------------------------------------------" | lolcat
 echo -e ""
 echo -e ""
